49148e5724d31647f08db0d7cf8f0009d6b47450
chidambaramct <49083278+chidambaramct@users.noreply.github.com>
2022-03-27 15:14:07 +0800

    Update my_dag.py
    removed unwanted commas

--- my_dag.py
+++ my_dag.py
@@ -14,20 +14,20 @@ dag = DAG(
     'my_dag',
     default_args=default_args,
     description='trial DAG',
-    schedule_interval=timedelta(days=1),
+    schedule_interval=timedelta(days=1)
 )
 
 t1 = BashOperator(
     task_id='print_date',
     bash_command='date',
-    dag=dag,
+    dag=dag
 )
 
 t2 = BashOperator(
     task_id='sleep',
     depends_on_past=False,
     bash_command='sleep 1',
-    dag=dag,
+    dag=dag
 )
 
 t1 >> t2
